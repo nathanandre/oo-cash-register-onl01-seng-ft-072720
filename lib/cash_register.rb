2e2e8de01aa8eca2e84825b685adf4e85d3e580a
Nathan Burns <nathanandreburns@gmail.com>
2020-08-09 21:12:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,19 +11,7 @@ class CashRegister
 
 
   def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = 
-    @total
-  end
+    
 
   def apply_discount()
     if @discount > 0
